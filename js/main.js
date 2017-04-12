bcc10b459f6e15eba2c3d1699a80eb80a343178b
angecroft <angeline.guignard@gmail.com>
2017-04-12 23:41:29 +0200

    UPDATE: video test

---
+++ js/main.js
@@ -0,0 +1,58 @@
+function loadProjects(){
+	// load Projets
+	var dfd = $.Deferred();
+
+	$.ajax({
+		url: "work.json",
+		dataType: 'json',
+		success: function(data) {
+			$("#work > .center_block").empty();
+			$(".load").hide();
+	        $.each(data.works, function(key, element){
+	        	// console.log(element);
+	        	var work = '<article id="'+key+'" class="work ';
+	        	$.each(element.categorie, function(i, categ){
+	        		work += categ+' '; 
+	        	});
+	        	work += element.size+'"';
+                work += ' style="background-image: url('+element.images[0]+');">';
+				work += '<a class="info" name="'+element.name+'" href="#/project/'+key+'">';
+				work += '<h3>'+element.name+'</h3>';
+				work += '<p>'+element.desc+'</p>'
+				work += '</a>';
+				work += '</article>';
+	    		$("#work > .center_block").prepend(work);
+	        });
+			$("#work > .center_block").prepend('<h2>Work</h2>');
+
+	        dfd.resolve(data);
+	    },
+		error: function(request, textStatus, errorThrown) {
+			dfd.reject(textStatus + ' : ' + errorThrown);
+		}
+	});
+
+	return dfd.promise();
+}
+
+$(document).ready(function() {
+	///////////////////////
+	/* Load project.json */
+	///////////////////////
+	loadProjects();
+
+	/////////////////
+	/* Back to top */
+	/////////////////
+	$(window).scroll(function() {
+		if($(window).scrollTop() > 100) {	
+			$("#backToTop").fadeIn(250);
+		} else {	
+			$("#backToTop").fadeOut(250);
+		}
+	});
+	$('#backToTop').click(function(event) {
+        $('html, body').animate({scrollTop : 0}, 'slow');
+        event.preventDefault();
+    });
+});
